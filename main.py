f480e433abbc90a4713e668bee631fefe6572e2c
Felipe Silva <felipe.estagiario@santiskin.com>
2025-01-09 16:21:06 +0000

    Create data folder for json file and inserted instances models, add function to read my json file on main.py and add items on my html from datase with basic styling

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 from flask import Flask, render_template, url_for, redirect, flash
-from flask_sqlalchemy import SQLAlchemy
-from models import Scores
+from models import db, Scores
+import json
+from sqlalchemy import text
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.../database/products.db'
-app.secret_key = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/products.db'
+app.secret_key = 'my_checkout_page'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-with app.app_context():
-    db.create_all()
+def load_json_data(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        for item in data:
+            existing_score = Scores.query.filter_by(title=item['title']).first()
+            if not existing_score:
+                score = Scores(**item)
+                db.session.add(score)
+        db.session.commit()
+
+def initialize_data():
+    with app.app_context():
+        load_json_data("data/seed_data.json")
+
+initialize_data()
 
 @app.route('/')
 def home():
@@ -20,9 +34,9 @@ def home():
 @app.route('/checkout/<int:id>')
 def checkout(id):
 
-    id = Scores.query.get(id)
+    score = Scores.query.get(id)
 
-    return render_template('checkout.html')
+    return render_template('checkout.html', score=score)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from main import db
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 class Scores(db.Model):
     __tablename__ = 'scores'
