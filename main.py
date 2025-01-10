ba96b9cc392e2e5a16089f262985865ba85af4ce
Felipe Silva <felipe.estagiario@santiskin.com>
2025-01-10 16:54:15 +0000

    Create POST request on homepage for filtering scores by composer's last name, add navbar and background image to checkout page and add icon to pages tab

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from flask import Flask, render_template, url_for, redirect, flash
+from flask import Flask, render_template, url_for, redirect, flash, request
 from models import db, Scores
 import json
-from sqlalchemy import text
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../database/products.db'
@@ -13,7 +12,7 @@ def load_json_data(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
         for item in data:
-            existing_score = Scores.query.filter_by(title=item['title']).first()
+            existing_score = Scores.query.filter((Scores.title == item['title']) & (Scores.composer == item['composer'])).first()
             if not existing_score:
                 score = Scores(**item)
                 db.session.add(score)
@@ -25,11 +24,22 @@ def initialize_data():
 
 initialize_data()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     scores = Scores.query.all()
 
-    return render_template('index.html', scores=scores)
+    unique_composer = sorted({score.composer.split()[-1] for score in scores})
+
+    if request.method == 'POST':
+        composer = request.form.get('composer')
+        last_name = composer.split()[-1]
+
+        if last_name and last_name != 'Filter by Composer' and last_name in unique_composer:
+            scores = Scores.query.filter(Scores.composer.like(f'% {last_name}')).all()
+
+        return render_template('index.html', scores=scores, unique_composer=unique_composer)
+
+    return render_template('index.html', scores=scores, unique_composer=unique_composer)
 
 @app.route('/checkout/<int:id>')
 def checkout(id):
